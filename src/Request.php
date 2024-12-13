df755f492e2aa95df7a4a0baf979d246abee3e38
TorusMvc <torusmvc@gmail.com>
2024-12-13 04:02:11 +0300

    Add files via upload

--- src/Request.php
+++ src/Request.php
@@ -8,6 +8,7 @@
  * License GPL
  *
  */
+
 namespace Pyramid;
 
 class Request {
@@ -18,21 +19,42 @@ class Request {
 	protected $file;
 	protected $server;
 	protected $input;
+	private $statusCode;
+	private $body;
+	private $headers;
 
-	public function __construct( $all = [], $get = [], $post = [], $file = [], $server = [], $input = '' ) {
-		$this->all    = $all ?: $_POST;
-		$this->get    = $get ?: $_GET;
-		$this->post   = $post ?: $_POST;
-		$this->file   = $file ?: $_FILES;
-		$this->server = $server ?: $_SERVER;
-		$this->input  = $input ?: file_get_contents( 'php://input' );
+	public function __construct( $all = [], $get = [], $post = [], $file = [], $server = [], $input = '', $statusCode = '', $body = '', $headers = [] ) {
+		$this->all        = $all ?: $_POST;
+		$this->get        = $get ?: $_GET;
+		$this->post       = $post ?: $_POST;
+		$this->file       = $file ?: $_FILES;
+		$this->server     = $server ?: $_SERVER;
+		$this->input      = $input ?: file_get_contents( 'php://input' );
+		$this->statusCode = $statusCode;
+		$this->body       = $body;
+		$this->headers    = $headers;
+	}
 
+	// HTTP yanıtının durum kodunu döndüren metod
+	public function getStatusCode() {
+		return $this->statusCode;
+	}
+
+	// Yanıt gövdesini döndüren metod
+	public function getBody() {
+
+		return $this->body;
+	}
+
+	// Yanıt başlıklarını döndüren metod
+	public function getHeaders() {
+		return $this->headers;
 	}
 
 	public function all() {
 		$this->all = $_POST;
 
-		return  $this->all;
+		return $this->all;
 	}
 
 	public function get( $key = null ) {
